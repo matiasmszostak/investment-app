0cc7e413ae7a7ce29968e19c09ec62c9055b5011
Matías Szostak <mszostak@asjservicios.com>
2024-08-27 23:40:17 -0300

    Added principal components and worked on the logic of the code

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+  
+
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      duration: +this.duration,
+      expectedReturn: +this.expectedReturn,
+      annualInvestment: +this.annualInvestment
+    });
+  }
+
+}
